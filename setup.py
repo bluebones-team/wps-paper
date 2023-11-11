35e9f11129b02b0480354e927bfbdde7a235bd2b
Cube X <shawn6304@qq.com>
2023-11-12 03:37:28 +0800

    py新增卸载功能

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
+from genericpath import isdir
 import os
 import shutil
-import json
 import logging
 import xml.etree.ElementTree as et
 import re
 
 
 def get_addon_info(path: str):
-    logging.info('获取配置信息')
+    logging.info('获取加载项名字和版本号')
     with open(path, encoding='utf-8') as f:
         text = f.read()
     name = re.search(r"name: '([\s\S]+?)'", text)
@@ -15,29 +15,41 @@ def get_addon_info(path: str):
     if name and version:
         return name.group(1), version.group(1)
     else:
-        raise '获取配置信息失败'
+        logging.error('获取失败')
+        quit()
+
+
+def remove_dir(parent_dir: str):
+    '''移除包含 NAME 的文件夹'''
+    for name in os.listdir(parent_dir):
+        if (NAME in name) and os.path.isdir(parent_dir + os.path.sep + name):
+            shutil.rmtree(parent_dir + os.path.sep + name)
+            logging.info('文件夹已移除')
+            return
+    logging.info('找不到相应文件夹，无需移除')
 
 
 def copy_dir(old_dir: str, new_dir: str):
-    logging.info('复制当前文件夹')
     parent_dir = os.path.dirname(new_dir)
-    for root_path, dir_names, file_names in os.walk(parent_dir):
-        for sub_dir_name in dir_names:
-            if NAME in sub_dir_name:
-                shutil.rmtree(parent_dir + os.path.sep + sub_dir_name)  # 删除文件夹
+    remove_dir(parent_dir)
+    logging.info('复制文件夹')
+    ignore_patterns = ['.git', THIS_NAME]
+    if os.path.exists('.gitignore'):
+        with open('.gitignore', encoding='utf-8') as f:
+            ignore_patterns += f.read().split('\n')
     shutil.copytree(
         old_dir,
         new_dir,
-        ignore=shutil.ignore_patterns('.git', '.gitignore', '*.config', '*.log', THIS_NAME),
+        ignore=shutil.ignore_patterns(*ignore_patterns),
     )
 
 
-def add_XML(path: str):
-    logging.info('添加加载项信息')
-    if not os.path.exists(path):
-        with open(path, 'w') as f:
+def add_XML(XML_path: str):
+    logging.info('注册加载项')
+    if not os.path.exists(XML_path):
+        with open(XML_path, 'w') as f:
             f.write('<jsplugins>\n</jsplugins>')
-    tree = et.parse(path)
+    tree = et.parse(XML_path)
     root = tree.getroot()
     nodes: list[et.Element] = list(
         filter(lambda node: node.attrib['name'] == NAME, root.findall('jsplugin'))
@@ -56,7 +68,38 @@ def add_XML(path: str):
         )
     else:
         nodes[0].attrib['version'] = VERSION
-    tree.write(path, encoding='utf-8')
+    tree.write(XML_path, encoding='utf-8')
+
+
+def remove_XML(XML_path: str):
+    logging.info('注销加载项')
+    if not os.path.exists(XML_path):
+        logging.info('找不到注册文件，无需卸载')
+        return
+    tree = et.parse(XML_path)
+    root = tree.getroot()
+    nodes: list[et.Element] = list(
+        filter(lambda node: node.attrib['name'] == NAME, root.findall('jsplugin'))
+    )
+    if nodes != []:
+        for node in nodes:
+            root.remove(node)
+    tree.write(XML_path, encoding='utf-8')
+
+
+def install():
+    if not os.path.exists(JSADDON_DIR):
+        os.makedirs(JSADDON_DIR)
+    new_dir_name = NAME + '_' + VERSION
+    copy_dir(os.path.dirname(__file__), JSADDON_DIR + new_dir_name)
+    add_XML(XML_PATH)
+    logging.info('安装成功, 当前文件夹可删除')
+
+
+def uninstall():
+    remove_dir(JSADDON_DIR)
+    remove_XML(XML_PATH)
+    logging.info('卸载成功, 当前文件夹可删除')
 
 
 if __name__ == '__main__':
@@ -70,12 +113,12 @@ if __name__ == '__main__':
     )
     try:
         JSADDON_DIR = os.environ['APPDATA'] + '\\kingsoft\\wps\\jsaddons\\'
+        XML_PATH = JSADDON_DIR + 'publish.xml'
         NAME, VERSION = get_addon_info('config.js')
-        if not os.path.exists(JSADDON_DIR):
-            os.makedirs(JSADDON_DIR)
-        new_dir_name = NAME + '_' + VERSION
-        copy_dir(os.path.dirname(__file__), JSADDON_DIR + new_dir_name)
-        add_XML(JSADDON_DIR + 'publish.xml')
-        logging.info('安装成功, 当前文件夹可删除')
+
+        {
+            '0': uninstall,
+            '1': install,
+        }[input('please input number 1 or 0\n(1:install, 0:uninstall)\n')]()
     except Exception as e:
         logging.error(e)
